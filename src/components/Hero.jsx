a630c4b65a462b794274f880d2fa47e7df40cb30
obsideen <97191699+Pritthish20@users.noreply.github.com>
2023-10-02 18:15:18 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -34,7 +34,7 @@ const Hero = () => {
           </p>
           <button>
               <a
-                href="#_"
+                href="https://docs.google.com/document/d/1QWTybhRht4KU0_Mywa-cnicrDZvR8kL6-xxS-vXgwQA/edit?usp=sharing"
                 class="px-5 py-2.5 relative rounded-2xl group font-medium text-white font-medium inline-block"
               >
                 <span class="absolute top-0 left-0 w-full h-full rounded-2xl opacity-50 filter blur-sm bg-gradient-to-br from-blue-100 to-blue-500"></span>
