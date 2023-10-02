48a69f37804f3d941ce3d705ac4b2c4e7ab3e5a0
obsideen <97191699+Pritthish20@users.noreply.github.com>
2023-10-02 18:11:57 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter } from "react-router-dom";
+<import { BrowserRouter } from "react-router-dom";
 import {
   About,
   Contact,
@@ -19,7 +19,7 @@ const App = () => {
           <Navbar />
           <Hero />
           <StarsCanvas/>
-          
+          <TowerCanvas/>
           <About />
           <Tech />
           <Works />
