8dccc16561634c32255b396ef4f8ab75dce4092c
obsideen <97191699+Pritthish20@users.noreply.github.com>
2023-10-02 17:48:17 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,7 +23,7 @@ export default {
       backgroundImage: {
         "hero-pattern":
           "url('/src/assets/3d-render-grunge-room-interior-with-foggy-atmosphere.jpg')",
-        "card": "url('/public/pokeball_-_free/textures/standardSurface2SG_emissive.png')",
+        "card": "url('/public/alpha_tower/textures/MAT_Metal_2_baseColor.png')",
         "circular":
           "repeating-radial-gradient(rgba(0,0,0,0.4) 40px,transparent 75px,transparent 230px)",
         "cirsmall":
