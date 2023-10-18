a4c127cf356f949e672207a2869c6892b71c2729
obsideen <97191699+Pritthish20@users.noreply.github.com>
2023-10-18 13:28:11 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
           <Navbar />
           <Hero />
           <StarsCanvas/>
-          <TowerCanvas/>
+        
           <About />
           <Tech />
           <Works />
